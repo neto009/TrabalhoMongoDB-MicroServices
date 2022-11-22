385c96531d909552495f26b421d5baa79e9670d8
Neto <solange.neto@mitraecp.com>
2022-11-22 13:13:06 -0300

    ajustes base

--- base/src/main/java/br/edu/iftm/base/BaseApplication.java
+++ base/src/main/java/br/edu/iftm/base/BaseApplication.java
@@ -1,5 +1,7 @@
 package br.edu.iftm.base;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -9,6 +11,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableEurekaClient
 @EnableMongoRepositories
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@OpenAPIDefinition(info = @Info(title = "Base API", version = "1.0", description = "Management Base API"))
 public class BaseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BaseApplication.class, args);
